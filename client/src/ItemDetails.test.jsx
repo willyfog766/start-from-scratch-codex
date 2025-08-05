dc688494f5b25e80034b64e0dfde83a9094284db
willyfog766 <vogalwork@gmail.com>
2025-08-04 22:18:43 -0500

    feat: improve item details display

--- client/src/ItemDetails.test.jsx
+++ client/src/ItemDetails.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react'
 import ItemDetails from './components/ItemDetails'
 
-test('renders item JSON data', () => {
+test('renders item details', () => {
   const item = { id: 'FOO', foo: 'bar' }
   render(<ItemDetails item={item} />)
   expect(screen.getByText(/Item Data/i)).toBeInTheDocument()
-  expect(screen.getByText(/"id": "FOO"/)).toBeInTheDocument()
+  expect(screen.getByText(/id:/i)).toBeInTheDocument()
+  expect(screen.getByText('FOO')).toBeInTheDocument()
 })
 
